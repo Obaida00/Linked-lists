37cdaf3559b0cdf3b9244b9adbcc3d983a7ae3a0
Obaida Almelhem <111151192+Obaida00@users.noreply.github.com>
2022-09-14 23:02:34 +0300

    Update LinkedList.py

--- LinkedList.py
+++ LinkedList.py
@@ -4,7 +4,7 @@ class Node(object):
         self.next = None
 
 
-class linked_list(object):
+class Linked_list(object):
     def __init__(self):
         self.head = Node()
 
@@ -28,14 +28,38 @@ class linked_list(object):
         curr = self.head
         while curr.next != None:
             lgth += 1
-            curr=curr.next
+            curr = curr.next
         return lgth
 
+    def extract(self, index):
+        if index >= self.length():
+            print("ERROR!! 'extract' Index out of range")
+            return
+        curr_index = 0
+        curr_node = self.head
+        while True:
+            curr_node = curr_node.next
+            if curr_index == index: return curr_node.data
+            curr_index += 1
+
+    def remove(self, index):
+        if index >= self.length():
+            print("ERROR!! 'remove' Index out of range")
+            return
+        curr_index = 0
+        curr_node = self.head
+        while True:
+            last_node = curr_node
+            curr_node = curr_node.next
+            if curr_index == index:
+                last_node.next = curr_node.next
+                return
+            curr_index += 1
 
 
+l = Linked_list()
 
-l = linked_list()
-
+# Appending/adding elements
 l.append(21)
 l.append(56)
 l.append(39)
@@ -43,5 +67,42 @@ l.append(28)
 l.append(12)
 l.append(31)
 
+# Displaying the list
+print('the linked list..')
 l.display()
-print(l.length())
+
+# Getting the length of the list
+print('\nthe length of the l-list is', l.length())
+
+# Extracting elements
+print('\nelement at index 0:', l.extract(0))
+print('element at index 1:', l.extract(1))
+print('element at index 2:', l.extract(2))
+print('element at index 3:', l.extract(3))
+print('element at index 4:', l.extract(4))
+print('element at index 5:', l.extract(5))
+
+# Removing elements
+l.remove(2)
+print('\nafter removing index 2')
+l.display()
+
+l.remove(3)
+print('after removing index 3')
+l.display()
+
+l.remove(3)
+print('after removing index 3')
+l.display()
+
+l.remove(1)
+print('after removing index 1')
+l.display()
+
+l.remove(1)
+print('after removing index 1')
+l.display()
+
+l.remove(0)
+print('after removing index 0')
+l.display()
